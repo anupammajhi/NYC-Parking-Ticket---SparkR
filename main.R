55e7f72a7100972d94b53e3dd18da8530da08405
Anupam Majhi <majhi.anupam@gmail.com>
2018-05-04 07:14:02 +0530

    Quick Fix

--- main.R
+++ main.R
@@ -327,3 +327,49 @@ NYC_All_Viol_Precinct_top5_peryear <- SparkR::sql("SELECT `Fiscal Year`,`Violati
                                                   WHERE rank <= 5") %>% collect()
 
 NYC_All_Viol_Precinct_top5_peryear 
+
+#  Fiscal Year Violation Precinct Frequency
+#         2016                  0   1807139
+#         2016                 19    545669
+#         2016                 18    325559
+#         2016                 14    318193
+#         2016                  1    299074
+#         2017                  0   1950083
+#         2017                 19    528317
+#         2017                 14    347736
+#         2017                  1    326961
+#         2017                 18    302008
+#         2015                  0   1455166
+#         2015                 19    550797
+#         2015                 18    393802
+#         2015                 14    377750
+#         2015                  1    302737
+
+# Plot
+
+NYC_All_Viol_Precinct_top5_peryear %>% ggplot(aes(as.character(`Violation Precinct`),Frequency)) +
+  geom_bar(aes(fill=as.character(`Violation Precinct`)),stat="identity") + 
+  facet_grid(.~`Fiscal Year`) +
+  labs(x="Violation Precinct", fill="Violation Precinct",title="Frequency of Violation Precinct getting parking tickets")
+  
+
+###########  3b. Issuing Precincts (this is the precinct that issued the ticket)
+
+NYC_All_Issue_Precinct_Grouped <- SparkR::sql("SELECT `Fiscal Year`,`Issuer Precinct`,count(`Issuer Precinct`) AS Frequency \ 
+                                              FROM NYC_All_View \
+                                              GROUP BY `Fiscal Year`,`Issuer Precinct`")
+
+createOrReplaceTempView(NYC_All_Issue_Precinct_Grouped ,"NYC_All_Issue_Precinct_Grouped_View")
+
+NYC_All_Issue_Precinct_top5_peryear <- SparkR::sql("SELECT `Fiscal Year`,`Issuer Precinct`, Frequency \
+                                                   FROM ( SELECT `Fiscal Year`,`Issuer Precinct`, Frequency, \
+                                                   dense_rank() OVER(PARTITION BY `Fiscal Year` ORDER BY Frequency DESC) AS rank \
+                                                   FROM NYC_All_Issue_Precinct_Grouped_View) \
+                                                   WHERE rank <= 5") %>% collect()
+
+NYC_All_Issue_Precinct_top5_peryear
+
+#  Fiscal Year Issuer Precinct Frequency
+#         2016               0   2067219
+#         2016              19    532298
+#         2016              18    317451
